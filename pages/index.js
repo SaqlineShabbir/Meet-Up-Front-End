361273f0f4e8c9676a33be361c49686922cb946f
Saqline Shabbir <saqline5869@gmail.com>
2022-06-15 00:58:01 +0600

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,20 @@
-import Head from "next/head";
-import Feed from "../components/Feed";
-import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
-import Widgets from "../components/Widgets";
+import Head from 'next/head';
+import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
+import Widgets from '../components/Widgets';
 
 export default function Home() {
   return (
     <div className="h-screen  bg-gray-100 overflow-hidden">
       <Head>
         <title>Meet Up</title>
-       {/* // hello  */}
+        {/* // hello  */}
       </Head>
       <Header />
       <main className="flex">
         <Sidebar />
         {/* newsFeed */}
-<<<<<<< HEAD
-        <Feed></Feed>
-=======
         <Widgets />
->>>>>>> f332012ede390f3cd9786bdcdb67889c4361135b
       </main>
     </div>
   );
