a0752f418e32ba52f3872e5d61d9f4e2505cbef0
Saqline Shabbir <saqline5869@gmail.com>
2022-06-15 17:20:39 +0600

    changes

--- components/Header.js
+++ components/Header.js
@@ -18,6 +18,7 @@ import useFirebase from '../hooks/UseFirebase';
 import HeaderIcon from './HeaderIcon';
 function Header() {
   const { logOut, user } = useFirebase();
+  console.log(user);
   return (
     <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg-px-5 shadow-md">
       {/* left */}
