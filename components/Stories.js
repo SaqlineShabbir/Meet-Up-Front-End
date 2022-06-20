937977cd2e2a89bcc958f8e45f7cdb24088d5bfc
Saqline Shabbir <saqline5869@gmail.com>
2022-06-20 20:28:24 +0600

    changes

--- components/Stories.js
+++ components/Stories.js
@@ -1,30 +1,30 @@
-import StoryCard from "./StoryCard";
+import StoryCard from './StoryCard';
 
 const stories = [
   {
-    name: "Jeff Bezoz",
-    src: "https://links.papareact.com/k2j",
-    profile: "https://links.papareact.com/f0p",
+    name: 'Jeff Bezoz',
+    src: 'https://links.papareact.com/k2j',
+    profile: 'https://links.papareact.com/f0p',
   },
   {
-    name: "Elon Musk",
-    src: "https://links.papareact.com/4zn",
-    profile: "https://links.papareact.com/kxk",
+    name: 'Elon Musk',
+    src: 'https://links.papareact.com/4zn',
+    profile: 'https://links.papareact.com/kxk',
   },
   {
-    name: "Bill Gates",
-    src: "https://links.papareact.com/4u4",
-    profile: "https://links.papareact.com/zvy",
+    name: 'Bill Gates',
+    src: 'https://links.papareact.com/4u4',
+    profile: 'https://links.papareact.com/zvy',
   },
   {
-    name: "Mark Zuckerberg",
-    src: "https://links.papareact.com/xql",
-    profile: "https://links.papareact.com/snf",
+    name: 'Mark Zuckerberg',
+    src: 'https://links.papareact.com/xql',
+    profile: 'https://links.papareact.com/snf',
   },
   {
-    name: "Sonny Sangha",
-    src: "https://links.papareact.com/zof",
-    profile: "https://links.papareact.com/l4v",
+    name: 'Sonny Sangha',
+    src: 'https://links.papareact.com/zof',
+    profile: 'https://links.papareact.com/l4v',
   },
 ];
 
